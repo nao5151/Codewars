35db319217ecdf50a04555837a3d5ea06e7f548a
Naoyuki Miyazaki <16092751+nao5151@users.noreply.github.com>
2020-01-25 14:13:50 +0900

    Create Scramblies.js

---
+++ 5/Scramblies.js
@@ -0,0 +1,21 @@
+function scramble(str1, str2) {
+  const map = {};
+  for(let i = 0; i < str1.length; i++) {
+    const s = str1[i];
+    if (map[s]) {
+      map[s]++;
+    } else {
+      map[s] = 1;
+    }
+  }
+  
+  for (let i = 0; i < str2.length; i++) {
+    const s = str2[i];
+    if (map[s] && map[s] > 0) {
+      map[s]--;
+    } else {
+      return false;
+    }
+  }
+  return true;
+}
